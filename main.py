d483ca7573512efc8357a608e98b9d998bd5ef00
Aditya Halder <144584943+AdityaHalder@users.noreply.github.com>
2023-11-08 09:02:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from pyrogram import Client, idle
+from pyrogram import filters
 
 api_id = 12380656
 api_hash = "d927c13beaaf5110f25c505b7c071273"
@@ -10,6 +11,14 @@ app = Client(
     api_hash = api_hash,
     bot_token = bot_token,
 )
+
+
+@app.on_message(filters.command("start") & filters.private)
+async def start_message(client, message):
+    message.reply_text(f"Hello, {message.from_user.mention}")
+
+
+
 
 
 app.start()
